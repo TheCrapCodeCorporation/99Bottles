a3476ca8355de383d802cb5d16ce554190178604
TheScrawl <TheScrawl@users.noreply.github.com>
2017-07-29 13:42:33 +1200

    set to run with default behavior if an error is encountered

--- bottleTerm.py
+++ bottleTerm.py
@@ -1,7 +1,14 @@
 import bottles
 import argparse
+import sys
 
-parser = argparse.ArgumentParser()
+class customParser(argparse.ArgumentParser):
+    def error(self, message):
+                sys.stderr.write('error: %s' % message + ', running with default behavior instead\n')
+                bottles.start()
+
+
+parser = customParser()
 parser.add_argument("bottles", help="Number of bottles to count down from", type=int)
 parser.add_argument("delay", help="Delay between lines printed", type=int)
 args = parser.parse_args()
