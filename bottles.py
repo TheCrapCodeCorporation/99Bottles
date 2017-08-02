2ead7ff24b705fda4888344110e0e395f6653e15
Yattaze <arthurhuia@gmail.com>
2017-08-03 11:05:00 +1200

    Added Check to fix "-1"

--- bottles.py
+++ bottles.py
@@ -3,7 +3,11 @@ import bottlesConfig
 
 def singMeAGoddamnSong(bottles, delay):
     while (bottles != -1):
-        print(str(bottles) + ' Bottles Of Beer on the Wall ' + str(bottles) + ' Bottles of beer, take one down, pass it around ' + str(bottles - 1) + ' Bottles Of Beer on the Wall.')
+        if(bottles == 0):
+            print ('No More Bottles of beer on the wall, no more bottles of beer. Go to the store get some more 12 bottles of beer on the wall.')
+            return
+        else:
+            print(str(bottles) + ' Bottles Of Beer on the Wall ' + str(bottles) + ' Bottles of beer, take one down, pass it around ' + str(bottles - 1) + ' Bottles Of Beer on the Wall.')
         bottles = bottles - 1
         time.sleep(delay)
 
